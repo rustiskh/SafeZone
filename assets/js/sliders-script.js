c8f2139e13d53f3505847aa37fc272d7243308be
AgataPit <agata-p@yandex.ru>
2023-03-05 12:05:05 +0300

    game-page-layout

--- assets/js/sliders-script.js
+++ assets/js/sliders-script.js
@@ -94,39 +94,35 @@ window.addEventListener("DOMContentLoaded", () => {
         ]
     });
 
-    // $(".review__slider-wrapper").slick({
-    //     arrows: true,
-    //     centerMode: false,
-    //     variableWidth: false,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 3,
-    //     // adaptiveHeight: true,
-    //     // dots: true,
-    //     prevArrow:
-    //         '<button type="button" class="slick-prev"><img src="icons/icon-landing-slider-arrow-left.svg"></button>',
-    //     nextArrow:
-    //         '<button type="button" class="slick-next"><img src="icons/icon-landing-slider-arrow-right.svg"></button>',
-    //     responsive: [
-    //         {
-    //             breakpoint: 1190,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 slidesToScroll: 2,
-    //                 centerMode: false,
-    //                 variableWidth: false,
-    //             },
-    //         },
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 // arrows: false,
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1,
-    //                 centerMode: true,
-    //                 variableWidth: true,
-    //             },
-    //         },
-    //     ],
-    // });
+    //слайдер на странице game-page/game-review//
+
+    $(".game-review__wrapper").slick({
+        arrows: true,
+        centerMode: true,
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="assets/icons/pages/game/arrow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="assets/icons/pages/game/arrow-right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 1290,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    infinite: true,
+                    arrows: false,
+                },
+            },
+        ]
+    });
+
+  
 });
 
